8960fafea7e6d5c92fb63bfedb507e35c1bfe3c4
D4tk1ller <thanhdatmu2003@gmail.com>
2022-09-16 13:11:54 +0700

    Update move Character

--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,10 @@
 module Test {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
 
+    opens uet.oop.bomberman.entities;
+    opens uet.oop.bomberman.graphics;
+    opens uet.oop.bomberman;
 }
