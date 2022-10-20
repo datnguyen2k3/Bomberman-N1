091cefbee3b598e332145232b32d1e1863f9d3c9
Thanh Tung <21020399@vnu,edu.vn>
2022-10-20 21:54:04 +0700

    sprite dead

--- src/uet/oop/bomberman/graphics/Sprite.java
+++ src/uet/oop/bomberman/graphics/Sprite.java
@@ -137,6 +137,10 @@ public class Sprite {
 	public static Sprite cat_left2 = new Sprite(DEFAULT_SIZE, 4, 6, SpriteSheet.tiles, 16, 16);
 	public static Sprite cat_left3 = new Sprite(DEFAULT_SIZE, 4, 7, SpriteSheet.tiles, 16, 16);
 
+	public static Sprite cat_dead = new Sprite(DEFAULT_SIZE, 4, 8, SpriteSheet.tiles, 16, 16);
+
+
+
 	//GHOST
 	public static Sprite ghost_right1 = new Sprite(DEFAULT_SIZE, 7, 5, SpriteSheet.tiles, 16, 16);
 	public static Sprite ghost_right2 = new Sprite(DEFAULT_SIZE, 7, 6, SpriteSheet.tiles, 16, 16);
@@ -145,6 +149,7 @@ public class Sprite {
 	public static Sprite ghost_left1 = new Sprite(DEFAULT_SIZE, 6, 5, SpriteSheet.tiles, 16, 16);
 	public static Sprite ghost_left2 = new Sprite(DEFAULT_SIZE, 6, 6, SpriteSheet.tiles, 16, 16);
 	public static Sprite ghost_left3 = new Sprite(DEFAULT_SIZE, 6, 7, SpriteSheet.tiles, 16, 16);
+	public static Sprite ghose_dead = new Sprite(DEFAULT_SIZE, 6, 8, SpriteSheet.tiles, 16, 16);
 
 	// COIN
 	public static Sprite red_coin_right = new Sprite(DEFAULT_SIZE, 12, 6, SpriteSheet.tiles, 16, 16);
